7f68f99ab4895d4d28bfbf3aceede28b57c80cf3
Jonathan-Schumann <137928634+Jonathan-Schumann@users.noreply.github.com>
2023-09-06 23:37:26 -0300

    :sparkles: Iniciando projeto
    versão inicial com página login e página de esqueci minha senha funcionando sem autenticação, página de cliente em construção

---
+++ Projeto-Desafio01/frontend/js/login.js
@@ -0,0 +1,25 @@
+// Capturando os campos
+let email = document.getElementById("exampleInputEmail")
+let senha = document.getElementById("exampleInputPassword")
+let btnEntrar = document.getElementById("btn-entrar");
+
+
+
+//Capturando evento de click
+btnEntrar.addEventListener('click', () => {
+    // 1º Pegar o email digitado
+    let userEmail = email.value;
+    // 2º Pegar a senha digitada
+    let userSenha = senha.value;
+    // 3º Validar se o email e senha estão corretos
+    if(!userEmail || !userSenha){
+        Swal.fire({
+            icon: 'error',
+            text: 'E-mail e senha obrigatórios!',
+          });
+        return;
+    }else if(userEmail == "admin@admin.com" && userSenha == "123456");{
+        location.href = "cliente.html";
+    }
+    
+});
